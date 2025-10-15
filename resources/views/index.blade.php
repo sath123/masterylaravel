441afed31a6fa54ba7811b8dee8aac530d0fb189
satheeskumar <satheeskumar@DESKTOP-EK6KIO3.localdomain>
2025-10-15 14:06:21 +0530

    template changes

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+This is my first page from laravel Blade template.
+
+@isset($name)
+Welcome {{$name}}
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index',[
+        'name' => 'Hi,Sathees'
+    ]);
+});
+
+Route::get('/hello', function (){
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function (){
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name){
+    return 'Hello '. $name . '!';
+});
+
+Route::fallback(function(){ 
+    return 'Still got somewhere !';
 });
